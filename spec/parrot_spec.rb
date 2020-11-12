8ed6e1070265e712b2a18328298f12d02620ca6f
Jess Pesale <jess_pesale@aol.com>
2020-11-12 16:19:14 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,8 +1,7 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
-parrot(sound)
-parrot("Pretty bird!")
+
 
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
